ef1b5c36fa35618ddf5f76297f905415b43c2528
zulfikarrahman30 <izulrahman86@gmail.com>
2022-02-24 13:02:11 +0700

    week 2

---
+++ Week2/PRAKTIKUM 2/web-react/like_button.js
@@ -0,0 +1,16 @@
+//... the starter code you pasted ...
+const e = React.createElement;
+
+function LikeButton(){
+    // Display a "Like" <button>
+    return e(
+    'button',
+    {
+        onClick: () => alert('berhasil')
+    },
+    'Like'
+    );
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
